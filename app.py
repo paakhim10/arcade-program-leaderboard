a38e40905774a89f9bd7a89f8cea93e216a334f2
paakhim10 <paakhim10@gmail.com>
2025-05-13 18:50:13 +0530

    adds may 13

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-date = "May 8, 2025"
-date_file = "08 May"
+date = "May 13, 2025"
+date_file = "13 May"
 # Set page layout to wide
 st.set_page_config(layout="wide")
 
